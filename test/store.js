e20130df7d21cebde2fa264efa21b1b8143d117b
Katja Lutz <mail@katjalutz.ch>
2016-10-23 22:37:00 +0200

    Added new tests

--- test/store.js
+++ test/store.js
@@ -12,4 +12,66 @@ describe('Store', function() {
       assert.equal('world', newStore.yet().hello);
     });
   });
+ 
+  describe('dispatch', function() {
+    it('should change state with returned state', function() {
+      var newStore = new alo.Store();
+      newStore.dispatch(function(state) {
+        state.hello = 'world';
+	return state;
+      });
+      assert.equal('world', newStore.yet().hello);
+    });
+    it('should not change state, when state was not returned', function() {
+      var newStore = new alo.Store();
+      newStore.dispatch(function(state) {
+        state.hello = 'world';
+      });
+      assert.equal(null, newStore.yet().hello);
+    });
+    it('should change state with dispatcher function', function() {
+      var newStore = new alo.Store();
+      newStore.dispatch(function(state, dispatcher) {
+        state.hello = 'world';
+        dispatcher(state);
+      });
+      assert.equal('world', newStore.yet().hello);
+    });
+    it('should not change state, when no state is returned by dispatcher', function() {
+      var newStore = new alo.Store();
+      newStore.dispatch(function(state, dispatcher) {
+        state.hello = 'world';
+      });
+      assert.equal(null, newStore.yet().hello);
+    });
+  });
+
+  describe('subscribe', function() {
+    it('should return an object of type Subscription', function() {
+      var newStore = new alo.Store();
+      var sub = newStore.subscribe(function() {});
+      assert.equal(true, sub instanceof alo.Subscription);
+    });
+    it('subscription function should be called on dispatch', function() {
+      var newStore = new alo.Store();
+      var status = false;
+      newStore.subscribe(function() {
+        status = true;
+      });
+      newStore.dispatch();
+      assert.equal(true, status);
+    });
+    it('subscription function should get new state of dispatch', function() {
+      var newStore = new alo.Store();
+      var state = {};
+      newStore.subscribe(function(newState) {
+        state = newState;
+      });
+      newStore.dispatch(function(state) {
+        state.hello = 'world';
+        return state;
+      });
+      assert.equal('world', state.hello);
+    });
+  });
 });
