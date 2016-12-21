3d214ee60b144b6cda49ad9827cc918d8ac8ce16
Katja Lutz <mail@katjalutz.ch>
2016-12-21 17:21:41 +0100

    Merge and updated dependencies

--- test/standard.test.js
+++ test/standard.test.js
@@ -3,7 +3,7 @@
 var standard = require('mocha-standard')
 
 describe('Coding style', function() {
-  this.timeout(5000)
+  this.timeout(7000)
   it('conforms to standard', standard.files([
     'lib/**/*.js'
   ]))
