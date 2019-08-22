168d264a9c6276e0dc73d49782dcd89c508b5d2a
Katja Lutz <mail@katjalutz.ch>
2019-08-22 18:04:46 +0200

    [FEATURE] Update to wald 0.2.1 and replace own GlobalsEntityCreatorDecorator

--- lib/alo/devtools/ioc.ts
+++ lib/alo/devtools/ioc.ts
@@ -2,34 +2,11 @@ import {
   SingletonEntityCreatorDecorator,
   EntityCreator,
   Ioc,
-  AbstractEntityCreatorDecorator,
-  BlueprintCreateFunctionOptions,
-  createBlueprint
+  createBlueprint,
+  GlobalsEntityCreatorDecorator,
+  createGlobalBlueprintCreateFn
 } from "wald";
 import { GlobalDevtoolsState } from ".";
-
-class GlobalsEntityCreatorDecorator extends AbstractEntityCreatorDecorator {
-  globals;
-  constructor(options: { globals: { [key: string]: any }; entityCreator }) {
-    super(options);
-
-    this.globals = options.globals;
-  }
-
-  create(options) {
-    options.create.globals = this.globals;
-
-    return this.entityCreator.create(options);
-  }
-}
-
-export const createGlobalBlueprintCreate = function<R = any>(
-  globalKey: string
-): (options: BlueprintCreateFunctionOptions) => R {
-  return function(options) {
-    return options.globals[globalKey] as any;
-  };
-};
 
 export const createIoc = function({ globalDevtoolsState }) {
   let entityCreator = new EntityCreator();
@@ -44,7 +21,7 @@ export const createIoc = function({ globalDevtoolsState }) {
 };
 
 export const GLOBAL_DEVTOOLS_STATE = createBlueprint({
-  create: createGlobalBlueprintCreate<GlobalDevtoolsState>(
+  create: createGlobalBlueprintCreateFn<GlobalDevtoolsState>(
     "globalDevtoolsState"
   )
 });
