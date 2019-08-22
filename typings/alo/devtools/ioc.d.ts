fd66243bd29ea5cb6212262bf6cc3cbee2e72a7b
Katja Lutz <mail@katjalutz.ch>
2019-08-22 18:10:11 +0200

    [FEATURE] Update version to 4.0.0-alpha.6 and build new dist

--- typings/alo/devtools/ioc.d.ts
+++ typings/alo/devtools/ioc.d.ts
@@ -1,8 +1,7 @@
-import { Ioc, BlueprintCreateFunctionOptions } from "wald";
+import { Ioc } from "wald";
 import { GlobalDevtoolsState } from ".";
-export declare const createGlobalBlueprintCreate: <R = any>(globalKey: string) => (options: BlueprintCreateFunctionOptions) => R;
 export declare const createIoc: ({ globalDevtoolsState }: {
     globalDevtoolsState: any;
 }) => Ioc;
-export declare const GLOBAL_DEVTOOLS_STATE: import("wald").Blueprint<(options: BlueprintCreateFunctionOptions) => GlobalDevtoolsState, import("wald").BlueprintMeta>;
+export declare const GLOBAL_DEVTOOLS_STATE: import("wald").Blueprint<(options: import("wald").BlueprintCreateFunctionOptions) => GlobalDevtoolsState, import("wald").BlueprintMeta>;
 //# sourceMappingURL=ioc.d.ts.map
