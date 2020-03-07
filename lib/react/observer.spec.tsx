e691a249cc15f6c36a9a6663cfc0a0cdffa59996
Katja Lutz <mail@katjalutz.ch>
2020-03-07 15:14:40 +0100

    test(react): added better renderCounts, added tests for observer unmount

--- lib/react/observer.spec.tsx
+++ lib/react/observer.spec.tsx
@@ -1,18 +1,24 @@
 import { assert } from "chai";
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { observable } from "alo";
 
 import { observer, observerWithState } from "./observer";
 
-let renderCount = 0;
+let helloWorldRenderCount = 0;
 const HelloWorld = function({ name = "world" }: { name?: string }) {
-  renderCount++;
+  helloWorldRenderCount++;
   return <div>hello {name}</div>;
 };
 
 describe("observer", function() {
+  afterEach(function() {
+    cleanup();
+  });
+
   const HelloWorldHOC = observer(HelloWorld);
 
+  let counterRenderCount = 0;
   const Counter = observerWithState(
     function() {
       return {
@@ -20,6 +26,8 @@ describe("observer", function() {
       };
     },
     function(_, state) {
+      counterRenderCount++;
+
       return (
         <>
           <button onClick={() => state.count++}>count</button>
@@ -29,14 +37,14 @@ describe("observer", function() {
     }
   );
 
-  it("should render hello world", function() {
+  it("should render hello world (twice)", function() {
+    helloWorldRenderCount = 0;
+
     const { getByText } = render(<HelloWorldHOC />);
     assert.exists(getByText("hello world"));
-  });
 
-  it("should have rendered before and after mount", function() {
     // This is an important test for ssr since componentDidMount will not be called there
-    assert.equal(renderCount, 2);
+    assert.equal(helloWorldRenderCount, 2);
   });
 
   it("should render hello mars", function() {
@@ -50,5 +58,29 @@ describe("observer", function() {
     assert.exists(getByText("1"));
     fireEvent.click(getByText("count"));
     assert.exists(getByText("2"));
+  });
+
+  it("should not rerender after observable change when its unmounted", function() {
+    const $state = observable({ name: "bob" });
+    let nameRenderCount = 0;
+    const NameObserver = observer(function() {
+      nameRenderCount++;
+
+      return <div>{$state.name}</div>;
+    });
+    const { unmount } = render(<NameObserver />);
+
+    // The count should be 2 from a basic render (it renders before and after mount)
+    assert.equal(nameRenderCount, 2);
+
+    // Before unmount we quickly test if the state subscription worked
+    $state.name = "alice";
+    assert.equal(nameRenderCount, 3);
+
+    unmount();
+
+    // Even though we change the name this shouldnt trigger a rerender after the unmount
+    $state.name = "john";
+    assert.equal(nameRenderCount, 3);
   });
 });
