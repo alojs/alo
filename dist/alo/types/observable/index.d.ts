4056fb1e7440b680f0874c0962f2a4a9e9897181
Katja Lutz <mail@katjalutz.ch>
2020-05-09 16:09:32 +0200

    chore: bump version to 4.0.0-alpha.23, build dist

--- dist/alo/types/observable/index.d.ts
+++ dist/alo/types/observable/index.d.ts
@@ -16,9 +16,9 @@ export declare const getProp: <T extends any, K extends keyof T>(obj: T, key: K)
 export declare const setProp: <T extends any, K extends keyof T>(obj: T, key: K, value: T[K]) => void;
 export declare function observable<T extends Record<string, any>>(obj: T, key?: any, parent?: any): Observable<T>;
 export declare function notify<T extends Observable<any>, K extends keyof T>(obj: T, key: K): void;
-export declare const batch: (fn: () => void) => void;
-export declare const batchStart: (batchId?: any) => any;
-export declare const batchEnd: (prevBatchId?: any) => void;
+export declare const batch: (fn: () => void, batchId?: any) => void;
+export declare const batchStart: (batchId?: any) => void;
+export declare const batchEnd: (batchId?: any) => void;
 export declare const computation: <T extends {}, P extends ComputationMap<T> = ComputationMap<T>>(propsObj: P, batch?: boolean) => [ComputationValues<P>, () => void];
 export declare const extract: (obj: any, deep?: boolean) => any;
 //# sourceMappingURL=index.d.ts.map
