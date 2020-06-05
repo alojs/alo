898c9c02a09705e3597de883ab124ceb64c430fe
Katja Lutz <mail@katjalutz.ch>
2020-06-05 19:12:15 +0200

    feat: implement default exports in create-type-proxies script

--- scripts/create-type-proxies.js
+++ scripts/create-type-proxies.js
@@ -2,11 +2,23 @@
 
 const { writeFile, pkgDir } = require("./lib");
 
-const createProxy = async (module) => {
-  await writeFile(
-    pkgDir(module, "dist", "main.d.ts"),
-    `export * from "./${module}/lib/main"`
-  );
+const createProxy = async (
+  module,
+  exportDefault = false,
+  exportNamed = true
+) => {
+  const mainPath = `./${module}/lib/main`;
+  let proxySrc = "";
+  if (exportNamed) {
+    proxySrc += `export * from "${mainPath}";\n`;
+  }
+
+  if (exportDefault) {
+    proxySrc += `import DefaultProxy from "${mainPath}";
+export default DefaultProxy;\n`;
+  }
+
+  await writeFile(pkgDir(module, "dist", "main.d.ts"), proxySrc);
 };
 
 (async () => {
