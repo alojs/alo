eea010be95fef653a2f42329ee4ee7553545e66f
Katja Lutz <mail@katjalutz.ch>
2020-07-25 20:20:48 +0200

    style: reformat example build script

--- scripts/build-example.js
+++ scripts/build-example.js
@@ -4,5 +4,15 @@ const { spawn, args, buildArgs, rmDir, pkgDir } = require("./lib");
 
 (async () => {
   await rmDir(pkgDir("example", "dist"), { recursive: true });
-  spawn("npx", ["microbundle", "--target", "node", "--format", "cjs", "--cwd", "example", ...buildArgs(), ...args]);
+  spawn("npx", [
+    "microbundle",
+    "--target",
+    "node",
+    "--format",
+    "cjs",
+    "--cwd",
+    "example",
+    ...buildArgs(),
+    ...args,
+  ]);
 })();
